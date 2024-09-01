e0cea9c80aae5411212ccb24ae17c6ed8bdd7976
jenmar02 <156864399+jenmar02@users.noreply.github.com>
2024-09-01 12:33:06 -0500

    added functionalities to the display cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,34 @@
+function updateTime() {
+  //San Salvador
+  let sanSalvadorElement = document.querySelector("#san-salvador");
+  let sanSalvadorDateElement = sanSalvadorElement.querySelector(".date");
+  let sanSalvadorTimeElement = sanSalvadorElement.querySelector(".time");
+  let sanSalvadorTime = moment().tz("America/El_Salvador");
+  sanSalvadorDateElement.innerHTML = sanSalvadorTime.format("MMMM Do, YYYY");
+  sanSalvadorTimeElement.innerHTML = sanSalvadorTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  //Chicago
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+  let chicagoTime = moment().tz("America/Chicago");
+  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do, YYYY");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Montevideo
+  let montevideoElement = document.querySelector("#montevideo");
+  let montevideoDateElement = montevideoElement.querySelector(".date");
+  let montevideoTimeElement = montevideoElement.querySelector(".time");
+  let montevideoTime = moment().tz("America/Montevideo");
+  montevideoDateElement.innerHTML = montevideoTime.format("MMMM Do, YYYY");
+  montevideoTimeElement.innerHTML = montevideoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
