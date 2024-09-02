c8daa5451b20878aa6f6715fba1e0474b91ced3a
jenmar02 <156864399+jenmar02@users.noreply.github.com>
2024-09-01 23:00:57 -0500

    added a city to main display

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,16 @@ function updateTime() {
     "h:mm:ss [<small>]A[</small>]"
   );
 
+  //Bangkok
+  let bangkokElement = document.querySelector("#bangkok");
+  let bangkokDateElement = bangkokElement.querySelector(".date");
+  let bangkokTimeElement = bangkokElement.querySelector(".time");
+  let bangkokTime = moment().tz("Asia/Bangkok");
+  bangkokDateElement.innerHTML = bangkokTime.format("MMMM Do, YYYY");
+  bangkokTimeElement.innerHTML = bangkokTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
   //Montevideo
   let montevideoElement = document.querySelector("#montevideo");
   let montevideoDateElement = montevideoElement.querySelector(".date");
